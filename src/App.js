230c39fa791463c180fc99b4810613db3d1bb3d9
Darius Seals <tristarmediagroupinc@gmail.com>
2022-03-07 20:44:23 -0500

    implemented userReducer and userActions to App and Header components

--- src/App.js
+++ src/App.js
@@ -6,42 +6,32 @@ import SignInAndSignUpPage from './pages/signin-and-signup/signin-and-signup.com
 import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.components';
 import { auth, createUserProfileDocument } from './firebase/firebase.utils'; 
+import { connect } from 'react-redux';
+import { setCurrentUser } from './redux/user/user.actions';
 
 
 
 class App extends React.Component {
-  constructor(props){
-    super(props);
-    this.state = {
-      currentUser: null
-    }
-  }
 
   unsubscribeFromAuth = null;
 
-  //onAuthStateChanged is a method that allows us to know when there is state change of the user in firbase project
+  //onAuthStateChanged is a method that allows us to know wh en there is state change of the user in firbase project
   //its an open messaging system between our app and firebase.  Whenever user updates - sign in/signout- we are given that user
   componentDidMount() {
+    const { setCurrentUser } = this.props;
     this.unsubscribeFromAuth = auth.onAuthStateChanged( async userAuth => {
 
       if (userAuth){
         const userRef = await createUserProfileDocument(userAuth);
 //Attaches a listener for DocumentSnapshot events. 
         userRef.onSnapshot(snapShot => {
-          this.setState({
-            currentUser: {
+          setCurrentUser({
               id: snapShot.id,
               ...snapShot.data()
-            }
-          }, () => {
-            console.log(this.state)
-          })
+          });
         });
-      } else {
-        this.setState({
-          currentUser: userAuth
-        })
-      }   
+      } 
+      setCurrentUser(userAuth);
     });
   }
 //unsubscribe when component unmounts to prevent memory leaks
@@ -52,7 +42,7 @@ class App extends React.Component {
   render(){
     return (
       <div className="App">
-      <Header currentUser={this.state.currentUser} />
+      <Header />
        <Routes>
         <Route path='/' element={<HomePage />} />
         <Route path='/signin' element={<SignInAndSignUpPage />} />
@@ -63,4 +53,8 @@ class App extends React.Component {
   }
 }
 
-export default App;
+const mapDispactchToProps = (dispatch) => ({
+  setCurrentUser: user => dispatch(setCurrentUser(user))
+})
+
+export default connect(null, mapDispactchToProps)(App);
