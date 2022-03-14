63eee00b3809394cc72bf2957e0ae09457fbc4a5
Darius Seals <tristarmediagroupinc@gmail.com>
2022-03-13 21:55:12 -0400

    added functionality to add item into catItems using redux reducer

--- src/components/collection-preview/collection-preview.jsx
+++ src/components/collection-preview/collection-preview.jsx
@@ -2,15 +2,15 @@ import React from 'react';
 import './collection-preview.scss';
 import CollectionItem from '../collection-item/collection-item.component';
 
-const CollectionPreview = ({title, items, id}) => (
+const CollectionPreview = ({title, items}) => (
     <div className='collection-preview'>
         <h1 className='title'>{title.toUpperCase()}</h1>
         <div className='preview'>
             {
                 items
                     .filter((item, idx) => idx < 4)
-                    .map(({id, ...restOfItemProps}) => (
-                    <CollectionItem key={id} {...restOfItemProps} />
+                    .map((item) => (
+                    <CollectionItem key={item.id} item={item} />
                 ))
             }
         </div>
